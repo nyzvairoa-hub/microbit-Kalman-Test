621cd1e4685e3475c9e948938955fa7d2964c735
nyzvairoa-hub <nyzvairoa@gmail.com>
2025-11-03 22:57:49 +0800

    Update bananaShims.cpp

--- bananaShims.cpp
+++ bananaShims.cpp
@@ -6,14 +6,17 @@ namespace banana{
     #define v1_mult 5
     #define v2_mult 10
 
+    //%
     int banana_add(int left, int right){
         return left + right;
     }
 
+    //%
     int banana_minus(int left, int right){
         return left - right;
     }
 
+    //%
     int banana_mult(int bananas){
         #if MICROBIT_CODAL
             return bananas * v2_mult;
@@ -22,6 +25,7 @@ namespace banana{
         #endif
     }
 
+    //%
     int bananas_v2_only(int bananas){
         #if MICROBIT_CODAL
             return bananas;
