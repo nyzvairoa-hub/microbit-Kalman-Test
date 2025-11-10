2e4593deb102722461581406f1a26cc24bd87ec9
nyzvairoa-hub <nyzvairoa@gmail.com>
2025-11-10 22:09:47 +0800

    change the print

--- bananaShims.cpp
+++ bananaShims.cpp
@@ -1,6 +1,6 @@
 #include "pxt.h" // Essential Micro:bit/MakeCode header
-#include "MicroBit.h"
-extern MicroBit uBit;
+//#include "MicroBit.h"
+//extern MicroBit uBit;
 
 using namespace pxt;
 
@@ -45,7 +45,7 @@ namespace banana{
     int bananaLoop(int n, int times){
         for(int i = 0; i <= times; i++){
             n += i;
-            uBit.serial.printf("Banana Loop %d: %d\n", i, n);
+            serial->printf("Banana Loop %d: %d\n", i, n);
         }
         return n;
     }
