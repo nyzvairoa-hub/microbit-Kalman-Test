f4920b6efef4e38f81c0327a2e12e56fadf91cb9
nyzvairoa-hub <nyzvairoa@gmail.com>
2025-11-17 18:56:53 +0800

    error not same variable

--- bananaShims.cpp
+++ bananaShims.cpp
@@ -44,7 +44,7 @@ namespace banana{
     int banana_getAccelX(){
 
         int x = uBit.accelerometer.getX(); 
-        uBit.serial.printf("Accel X: %d mg\r\n", x_value);
+        uBit.serial.printf("Accel X: %d mg\r\n", x);
         return x;
     }
 
