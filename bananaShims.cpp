76c74e842602739c50364ee8c709371e1d10b16e
nyzvairoa-hub <nyzvairoa@gmail.com>
2025-11-28 01:54:25 +0800

    up

--- bananaShims.cpp
+++ bananaShims.cpp
@@ -17,22 +17,21 @@ using namespace pxt;
 namespace banana {
 
     // --- GLOBAL VARIABLES ---
-    // Must be static to be fiber-safe
     static bool banana_loop_true = false; 
     static int globalSpeed = 0;
     static int globalDir = 0;
 
-    // --- 1. HELPER FUNCTIONS (Logic INSIDE namespace) ---
+    // --- 1. HELPER FUNCTIONS ---
 
     void i2cWrite(uint8_t reg, uint8_t value){
         uint8_t buff[2];
         buff[0] = reg;
         buff[1] = value;
-
+        
         #if MICROBIT_CODAL
-            int res = uBit.i2c.write(PCA9685_ADDRESS, buff, 2);
+            int res = uBit.i2c.write(PCA9685_ADDRESS, buff, 2, false);
         #else
-            int res = uBit.i2c.write(PCA9685_ADDRESS, (char*)buff, 2);
+            int res = uBit.i2c.write(PCA9685_ADDRESS, (char*)buff, 2, false);
         #endif
         
         if (res != 0) {
@@ -44,12 +43,12 @@ namespace banana {
         uint8_t buff[3];
         buff[0] = reg;
         buff[1] = value & 0xFF; 
-        buff[2] = (value >> 8) & 0xFF;
-
+        buff[2] = (value >> 8) & 0xFF; 
+        
         #if MICROBIT_CODAL
-            uBit.i2c.write(PCA9685_ADDRESS, buff, 3);
+            uBit.i2c.write(PCA9685_ADDRESS, buff, 3, false);
         #else
-            uBit.i2c.write(PCA9685_ADDRESS, (char*)buff, 3);
+            uBit.i2c.write(PCA9685_ADDRESS, (char*)buff, 3, false);
         #endif
     }
 
@@ -67,71 +66,74 @@ namespace banana {
         if(dir == FORWARD){
             set_pwm(M1_IN1_CHANN, 0, duty_cycle);
             set_pwm(M1_IN2_CHANN, 0, 0);
-            //uBit.serial.printf("M1: FWD %d\r\n", speed);
+            uBit.serial.printf("M1: FWD %d\r\n", speed);
         } else if(dir == BACKWARD){
             set_pwm(M1_IN1_CHANN, 0, 0);
             set_pwm(M1_IN2_CHANN, 0, duty_cycle);
-            //uBit.serial.printf("M1: REV %d\r\n", speed);
+            uBit.serial.printf("M1: REV %d\r\n", speed);
         } else{
             set_pwm(M1_IN1_CHANN, 0, 0);
             set_pwm(M1_IN2_CHANN, 0, 0);
-            //uBit.serial.printf("M1: STOP\r\n");
+            uBit.serial.printf("M1: STOP\r\n");
         }
     }
 
+    // --- REFINED SCANNER ---
     void scanI2C() {
         uBit.serial.printf("\r\n--- STARTING I2C SCAN ---\r\n");
         int devices_found = 0;
         
-        // Scan standard 7-bit addresses (0x08 to 0x77) to avoid reserved blocks
+        // Scan standard 7-bit addresses
         for (int i = 8; i < 120; i++) {
             uint8_t dummy = 0;
             #if MICROBIT_CODAL
-                // V2: address is shifted left by 1 (8-bit address)
-                int res = uBit.i2c.write(i << 1, &dummy, 0); 
+                int res = uBit.i2c.write(i << 1, &dummy, 0, false); 
             #else
-                int res = uBit.i2c.write(i << 1, (char*)&dummy, 0);
+                int res = uBit.i2c.write(i << 1, (char*)&dummy, 0, false);
             #endif
             
             if (res == 0) {
                 uBit.serial.printf("DEVICE FOUND AT: 0x%x (%d)\r\n", i, i);
                 devices_found++;
-                uBit.sleep(10); // Print delay
+                uBit.sleep(10); 
             }
         }
-        
-        if (devices_found == 0) {
-            uBit.serial.printf("NO DEVICES FOUND! Check Power/Connections.\r\n");
-        } else {
-            uBit.serial.printf("Scan Complete. Found %d devices.\r\n", devices_found);
-        }
+        uBit.serial.printf("Scan Complete. Found %d devices.\r\n", devices_found);
         uBit.serial.printf("-------------------------\r\n");
     }
 
     void i2cInit(){
+        scanI2C(); // Run scan first
+        
+        // Give the bus a moment to settle after scanning
+        uBit.sleep(100); 
 
-        scanI2C();
-        fiber_sleep(50);
+        // Try initialization sequence
         i2cWrite(PCA9685_MODE1, 0x00);
-        fiber_sleep(10);
+        uBit.sleep(10);
         i2cWrite(PCA9685_MODE1, 0x10); 
         i2cWrite(PCA9685_PRESCALE, 0x79); 
         i2cWrite(PCA9685_MODE1, 0x00); 
-        fiber_sleep(10);
+        uBit.sleep(10);
         i2cWrite(PCA9685_MODE1, 0xa0); 
     }
 
-    // --- 2. FIBER LOOP (Logic INSIDE namespace) ---
+    // --- 2. FIBER LOOP ---
     void banana_loop(){
         while(banana_loop_true){
             control_motor1(globalSpeed, globalDir);
-            fiber_sleep(10);
+            uBit.sleep(100);
         }
         control_motor1(0, 0);
     }
 
-    // --- 3. EXPORTED SHIMS (Logic INSIDE namespace) ---
-    // The parser needs the code block { ... } to be right here!
+    // --- 3. EXPORTED SHIMS ---
+
+    //%
+    void banana_getAccelX(){
+        int x = uBit.accelerometer.getX(); 
+        uBit.serial.printf("Accel X: %d mg\r\n", x);
+    }
 
     //%
     void banana_run(int speed, int dir){
@@ -142,7 +144,6 @@ namespace banana {
             banana_loop_true = true;
             create_fiber(banana_loop);
         } else {
-            // Update speed even if loop is already running
             globalSpeed = speed;
             globalDir = dir;
         }
@@ -155,5 +156,11 @@ namespace banana {
         globalSpeed = 0;
         banana_loop_true = false;
     }
+    
+    //%
+    void banana_multPrint(int n, int m){
+        int x = n * m;
+        uBit.serial.printf("Mult: %d\r\n", x);
+    }
 
-} // --- END OF NAMESPACE ---
+} // --- END NAMESPACE ---
