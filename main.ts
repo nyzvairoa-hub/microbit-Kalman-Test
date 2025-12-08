fe7f5ef251304bd82f0bf9d7e67f10d13acfb46f
nyzvairoa-hub <nyzvairoa@gmail.com>
2025-12-08 20:25:19 +0800

    Update pxt.json, main.ts, main.py, main.blocks

--- main.ts
+++ main.ts
@@ -1,23 +1,11 @@
 banana.startMotor()
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
-basic.forever(function () {
-    huskylens.request()
+basic.forever(function on_forever() {
     if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        banana.huskyLensData(
-        huskylens.readeBox(1, Content1.xCenter),
-        huskylens.readeBox(1, Content1.yCenter),
-        huskylens.readeBox(1, Content1.width),
-        huskylens.readeBox(1, Content1.height),
-        true
-        )
+        banana.huskyLensData(huskylens.readBox_s(Content3.xCenter), huskylens.readBox_s(Content3.yCenter), huskylens.readBox_s(Content3.width), huskylens.readBox_s(Content3.height), true)
     } else {
-        banana.huskyLensData(
-        0,
-        0,
-        0,
-        0,
-        false
-        )
+        banana.huskyLensData(0, 0, 0, 0, false)
     }
+    
 })
