6db62b5c64bccadd0b62ff6f00315d191ad8bbe0
nyzvairoa-hub <nyzvairoa@gmail.com>
2025-12-08 20:50:55 +0800

    up

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-banana.startMotor()
-huskylens.initI2c()
-huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
-basic.forever(function on_forever() {
-    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        banana.huskyLensData(huskylens.readBox_s(Content3.xCenter), huskylens.readBox_s(Content3.yCenter), huskylens.readBox_s(Content3.width), huskylens.readBox_s(Content3.height), true)
-    } else {
-        banana.huskyLensData(0, 0, 0, 0, false)
-    }
-    
-})
+
