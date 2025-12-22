b0509251745ccb8a0ed9257366e7143a4100a58a
nyzvairoa-hub <nyzvairoa@gmail.com>
2025-12-22 16:55:39 +0800

    Update pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let y = 0
+let x = 0
+banana.startMotor()
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
 basic.forever(function () {
-    banana.bananaMultPrint(3, 3)
+    huskylens.request()
+    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        x = huskylens.readeBox(1, Content1.width)
+        y = huskylens.readeBox(1, Content1.yCenter)
+        banana.huskyLensData(x, y, true)
+    } else {
+        banana.huskyLensData(-1, -1, false)
+    }
 })
