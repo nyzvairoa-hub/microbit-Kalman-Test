4b939df21f7c82ffeca1140c1e1e6cfb0e75e8d0
nyzvairoa-hub <nyzvairoa@gmail.com>
2026-01-15 18:44:28 +0800

    update

--- KalmanFilterHeader.h
+++ KalmanFilterHeader.h
@@ -12,6 +12,7 @@ class PVKalman{
         // covarianve variable
         float p_xx;
         float p_vv;
+        float p_xv;
 
         // parameter tuning
         float q_pos;
@@ -26,6 +27,7 @@ class PVKalman{
             // initial covariance
             p_xx = 100.0;
             p_vv = 100.0;
+            p_xv = 0.0;
 
             // tuning parameters
             r_measure = measurement_noise;
@@ -39,14 +41,15 @@ class PVKalman{
             x += v * dt;
 
             // extrapolate uncertainty (covariance)
-            p_xx += dt * dt * p_vv + q_pos;
-            p_vv += q_vel;
+            p_xx += 2 * p_xv * dt + p_vv * dt * dt + q_pos;
+            p_xv += p_vv * dt;
+            p_vv += q_vel; // q_vel is the process noise
         }
 
         void update(float measurement){
             // compute kalman gain
             float k_x = p_xx / (p_xx + r_measure);
-            float k_v = p_vv / (p_vv + r_measure);
+            float k_v = p_xv / (p_xx + r_measure);
 
             // update state estimate
             // new = old + gain * (measured - old)
@@ -56,9 +59,14 @@ class PVKalman{
             // update velocity based on position error
             v = v + k_v * inno;
 
-            // update uncertainty
-            p_xx = (1 - k_x) * p_xx;
-            p_vv = (1 - k_v) * p_vv;
+            float p_xx_old = p_xx;
+            float p_xv_old = p_xv;
+
+            // update covariance
+            p_xx = (1.0f - k_x) * p_xx_old;
+            p_xv = (1.0f - k_x) * p_xv_old;
+            p_vv = p_vv - (k_v * p_xv_old);
+
         }
 };
 
