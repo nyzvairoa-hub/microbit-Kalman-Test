2ce835558db20c5c8ee3d6b1cbae723f6f517d5d
nyzvairoa-hub <nyzvairoa@gmail.com>
2026-04-21 21:45:36 +0800

    Update pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     basic.pause(2000)
+    banana.setAutoMode(true)
+    banana.startMotor()
     while (true) {
         now = control.millis()
         dt_actual = now - lastTime
@@ -29,14 +31,19 @@ let dt_actual = 0
 let now = 0
 let lastTime = 0
 lastTime = control.millis()
-banana.startMotor()
-banana.setAutoMode(true)
 banana.pidValue(
-0.4,
-2.5,
+0.5,
+2,
 0.5,
 0.5
 )
-banana.KalmanFilterValues(9, 1)
+banana.KalmanFilterValues(
+1.368948849,
+0.04624,
+1,
+3,
+0.1,
+0.1
+)
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
